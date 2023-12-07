0d16304ed48e7dd29a3b991371cf2e0066c2b13e
tanduy222002 <79954594+tanduy222002@users.noreply.github.com>
2023-12-07 11:42:15 +0700

    fetch api in admin

--- src/Component/quang/order.js
+++ src/Component/quang/order.js
@@ -17,7 +17,7 @@ import {getProductById} from '../../api/userApi'
 import {setOrderStatus} from '../../api/adminApi'
 import {deleteOrder} from '../../api/adminApi'
 function ShowOrder(props) {
-  const [data, setData] = useState([]);
+  const [order, setOrder] = useState([]);
   const [customerInfo, setCustomerInfo] = useState([]);
   const [detailOrder, setDetailOrder] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -50,7 +50,7 @@ function ShowOrder(props) {
       await deleteOrder(token,orderId);
       const res = await getOrderByStatus("Confirm", localStorage.getItem('token'));
       console.log(res);
-      setData(res);
+      setOrder(res);
       // Xử lý logic sau khi gọi API thành công (nếu cần)
 
     } catch (error) {
@@ -63,31 +63,42 @@ function ShowOrder(props) {
     const fetchData = async () => {
       try {
         const res = await getOrderByStatus("Confirm", localStorage.getItem('token'));
-        console.log(res);
-        if (res && Array.isArray(res)) {
-          setData(res);
-          
-          const customerInfoPromises = res.map(async (order) => {
+        const data=res.data;
+        console.log(data);
+        if (data && Array.isArray(data)) {
+          console.log("is array")
+          setOrder(data);
+          console.log(order)
+
+         
+          const customerInfoPromises = data.map(async (order) => {
+            console.log("this is check")
+            console.log(order)
             const userInfo = await getUser(order.user_id, localStorage.getItem('token'));
+            const userData=userInfo.data;
             return {
               id: order.user_id,
-              name: userInfo[0].name,
-              phone: userInfo[0].phone,
-              email: userInfo[0].email,
-              address: userInfo[0].address
+              name: userData.name,
+              phone: userData.phone,
+              email: userData.email,
+              address: userData.address
             };
           });
   
-          const detailOrderPromises = res.map(async (order) => {
+          const detailOrderPromises = data.map(async (order) => {
             const detail = await viewDetailOrder(localStorage.getItem('token'), order.id);
-            return detail;
+            return detail.data.order_detail;
           });
   
           const customerInfo = await Promise.all(customerInfoPromises);
+       
           setCustomerInfo(customerInfo);
-  
+          console.log(customerInfo);
+         
           const detailOrder = await Promise.all(detailOrderPromises);
+          
           setDetailOrder(detailOrder);
+          console.log(detailOrder)
         }
       } catch (error) {
         console.error('Error:', error);
@@ -97,40 +108,43 @@ function ShowOrder(props) {
     fetchData();
   }, []);
   
-  useEffect(() => {
-    const updateProductInfo = async () => {
-      try {
-        const updatedDetailOrder = await Promise.all(
-          detailOrder.map(async (order) => {
-            for (let item of order) {
-              const productInfo = await getProductById(item.product_id);
-              if (productInfo) {
-                const updatedItem = {
-                  ...item,
-                  product_name: productInfo.data.name,
-                  product_price: productInfo.data.price* (100-productInfo.data.sale_percent)/100
-                };
-                const index = order.findIndex((detail) => detail.product_id === item.product_id);
-                if (index !== -1) {
-                  order[index] = updatedItem;
-                }
-              }
-            }
-            return order;
-          })
-        );
+  // useEffect(() => {
+  //   const updateProductInfo = async () => {
+  //     try {
+  //       const updatedDetailOrder = await Promise.all(
+  //         detailOrder.map(async (order) => {
+  //           for (let item of order) {
+  //             const productInfo = await getProductById(item.product_id);
+  //             if (productInfo) {
+  //               const updatedItem = {
+  //                 ...item,
+  //                 product_name: productInfo.data.name,
+  //                 product_price: productInfo.data.price* (100-productInfo.data.sale_percent)/100
+  //               };
+  //               const index = order.findIndex((detail) => detail.product_id === item.product_id);
+  //               if (index !== -1) {
+  //                 order[index] = updatedItem;
+  //               }
+  //             }
+  //           }
+  //           return order;
+  //         })
+  //       );
   
-        setDetailOrder(updatedDetailOrder);
-      } catch (error) {
-        console.error('Error updating product info:', error);
-      }
-    };
+  //       setDetailOrder(updatedDetailOrder);
+  //     } catch (error) {
+  //       console.error('Error updating product info:', error);
+  //     }
+  //   };
   
-    if (detailOrder.length > 0) {
-      updateProductInfo();
-    }
-  }, [detailOrder]);
-  
+  //   if (detailOrder.length > 0) {
+  //     updateProductInfo();
+  //   }
+  // }, [detailOrder]);
+  const calculateTotalPrice = (detailOrder)=>{
+    console.log(detailOrder)
+   return detailOrder.reduce((total, order) => total + order.price, 0).toLocaleString();
+  }
   return (
     <div style={{ padding: "0px" }}>
       <HeaderAdmin/>
@@ -163,7 +177,7 @@ function ShowOrder(props) {
             </div>
 
             <Container>
-            {data.map((staff,index) => (
+            {order.map((staff,index) => (
               <Row style={{backgroundColor: "lightgrey", marginBottom: "60px", height: "350px", borderRadius: "20px"}}>
                 <Col md={6}>
                   <div key={index} style={{padding: "20px 5px"}}>
@@ -199,21 +213,22 @@ function ShowOrder(props) {
                         <th>Số lượng</th>
                       </tr>
                     </thead>
+                    {console.log(detailOrder[index])}
                     {detailOrder[index] && (
                     <tbody>
                       {detailOrder[index].map((detail) => (
                         <tr>
-                          <td>{detail.product_name}</td>
-                          <td>{detail.product_price? detail.product_price.toLocaleString():''}</td>
+                          <td>{detail.name}</td>
+                          <td>{detail.price? detail.price.toLocaleString():''}</td>
                           <td>{detail.count}</td>
                         </tr>
                       ))}
                     </tbody>
                     )}
                   </Table>
-
+                  {/* {staff.sum_price.toLocaleString()} */}
                   <div style={{ position: "absolute", right: "16px" }}>
-                    <p style={{fontWeight: "bold", fontSize: "20px"}}>Tổng tiền: {staff.sum_price.toLocaleString()}</p>
+                   {detailOrder[index] && <p style={{fontWeight: "bold", fontSize: "20px"}}>Tổng tiền:{calculateTotalPrice(detailOrder[index])} </p>}
                       <div style={{display: "flex", flexDirection: "row", gap: "20px"}}>
                       <Button variant="success" href="/adm_man_deliver" onClick={() => handleConfirm(staff.id)} disabled={loading}>Xác nhận</Button>
                       <Button variant="danger" onClick={() => deleteOrderr(staff.id)}>Hủy bỏ</Button>
